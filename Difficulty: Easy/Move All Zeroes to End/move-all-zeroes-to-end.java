518f58222aa2e246fccd555e052e86d82b7d3e20
Sudeep-mudrady2004 <19609sudeep@gmail.com>
2025-04-03 22:50:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n=arr.length;
+        int temp[]=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                temp[j++]=arr[i];
+            }
+        }
+        while(j<n)
+        {
+            temp[j++]=0;
+        }
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=temp[i];
+            }
+        
+        
+    }
+}
