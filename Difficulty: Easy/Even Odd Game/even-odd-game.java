1114fc9e947920d15b58012f5b6f774cd76bd891
Sudeep-mudrady2004 <19609sudeep@gmail.com>
2025-04-15 22:10:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,25 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Friend");
+        }
+        else
+        {
+            System.out.println("You");
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
