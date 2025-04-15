861ed3c4d8b3a5dc144be10739fb8eeecf316d21
Sudeep-mudrady2004 <19609sudeep@gmail.com>
2025-04-15 22:29:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+         int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        switch(a)
+        {
+            case 1:System.out.println("One");
+                    break;
+            case 2:System.out.println("Two");
+                    break;
+            case 3:System.out.println("Three");
+                    break;
+            case 4:System.out.println("Four");
+                    break;
+            case 5:System.out.println("Five");
+                    break;
+            case 6:System.out.println("Six");
+                    break;
+            case 7:System.out.println("Seven");
+                    break;
+            case 8:System.out.println("Eight");
+                    break;
+            case 9:System.out.println("Nine");
+                    break;
+            default:
+                    System.out.println("Unknown");
+                    break;
+        
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
